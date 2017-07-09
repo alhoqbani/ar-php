05b2a98cd273d272570c0aca98b163cd8bad3070
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-09 07:44:40 -0700

    Add test for returned format

--- tests/Arabic/DateTest.php
+++ tests/Arabic/DateTest.php
@@ -37,4 +37,13 @@ class DateTest extends AbstractTestCase
         $actualYear = $this->date->date('Y', strtotime('2017'));
         $this->assertEquals(1438, $actualYear);
     }
+    
+    /** @test */
+    public function it_returns_hijri_date_in_requested_format_when_given_timestamp()
+    {
+        $timestamp = 1499611163;
+        $expectedString = 'الأحد 14 شوال 1438 07:39:23 صباحاً';
+        $actualString = $this->date->date('l dS F Y h:i:s A', $timestamp);
+        $this->assertEquals($expectedString, $actualString);
+    }
 }
