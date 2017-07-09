4b15df4e01afaf42f866fb374a59ba35e56314db
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-09 08:39:01 -0700

    Add test for number of days in a month and correction diff

--- tests/Arabic/MktimeTest.php
+++ tests/Arabic/MktimeTest.php
@@ -44,5 +44,16 @@ class MktimeTest extends AbstractTestCase
         $this->assertEquals(336126615, $actualTimestamp);
     }
     
+    /** @test */
+    public function it_makes_time_correction()
+    {
+        $actualDiff = $this->mktime->mktimeCorrection(10, 1430);
+        $this->assertEquals(-1, $actualDiff);
+    }
     
+    /** @test */
+    public function it_returns_hijri_count_of_days_in_givin_hijri_month()
+    {
+        $this->assertEquals(29, $this->mktime->hijriMonthDays(7, 1438));
+    }
 }
