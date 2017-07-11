c31a8ca665f55c5b7dafcdc20572146e416a946f
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-11 01:27:17 -0700

    Test arNum and enNum

--- public/index.php
+++ public/index.php
@@ -2,5 +2,11 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$Arabic = new I18N_Arabic('Normalise');
+$Arabic = new I18N_Arabic('Transliteration');
 
+
+echo $text = $Arabic->enNum('323423' . ' Text in between ' . '٥٥٥٥' . 'ورقم ٢٣٤,٣٤ مع نص عربي');
+dump($text);
+
+echo $text = $Arabic->arNum(345 . ' Text in between ' . 34543 . 'مع نص١٢٣٢١ عربي');
+dump($text);
--- tests/Arabic/TransliterationTest.php
+++ tests/Arabic/TransliterationTest.php
@@ -46,4 +46,24 @@ class Transliteration extends AbstractTestCase
     
     }
     
+    /** @test */
+    public function it_renders_Arabic_numbers_as_html_entites()
+    {
+        $actualText = $this->transliterator->enNum(345 . ' Text in between ' . 34543 . 'مع نص عربي');
+        $expectText = '&#x33;&#x34;&#x35; Text in between &#x33;&#x34;&#x35;&#x34;&#x33;مع نص عربي';
+        
+        $this->assertEquals($expectText, $actualText);
+        
+    }
+    
+    /** @test */
+    public function it_renders_numbers_into_indian_digits_as_html_entities()
+    {
+        $actualText = $this->transliterator->arNum('323423' . ' Text in between ' . '23434' . 'ورقم ٢٣٤,٣٤ مع نص عربي');
+        $expectText = '&#x0663;&#x0662;&#x0663;&#x0664;&#x0662;&#x0663; Text in between &#x0662;&#x0663;&#x0664;&#x0663;&#x0664;ورقم ٢٣٤,٣٤ مع نص عربي';
+
+        $this->assertEquals($expectText, $actualText);
+        
+    }
+    
 }
