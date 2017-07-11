d75600535c0e6756b47418d89568864d014dca0d
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-11 10:24:19 -0700

    Add tests to IdentifierTest

--- tests/Arabic/IdentifierTest.php
+++ tests/Arabic/IdentifierTest.php
@@ -24,4 +24,33 @@ class IdentifierTest extends AbstractTestCase
     {
         $this->assertInstanceOf(I18N_Arabic_Identifier::class, $this->identifier->myObject);
     }
+    
+    /** @test */
+    public function it_identify_Arabic_text_in_a_given_UTF8_multi_language_string()
+    {
+        $text = <<<END
+كل شعوب العالم تفضل السلام علي الحرب The people of the world prefer peace to war
+The Internet Internationalization
+(I18N) community, which values diversity and human life everywhere
+والتي تأخذ بعين
+التقدير الاختلافات الثقافية والعادات الحياتية
+מעדיפים את השלום על-פני המלחמה והם
+مجموعة تدويل الإنترنت
+END;
+        $expectedArray = [0, 67, 212, 328, 391,];
+        
+        $actualArray = $this->identifier->identify($text);
+        
+        $this->assertEquals($expectedArray, $actualArray);
+    }
+    
+    /** @test */
+    public function it_detects_Arabic_text()
+    {
+        $this->assertTrue($this->identifier->isArabic('كل شعوب العالم تفضل السلام علي الحرب'));
+        $this->assertFalse($this->identifier->isArabic('The people of the world prefer peace to war'));
+        $this->assertFalse($this->identifier->isArabic('The people of the world كل شعوب العالم'));
+    }
+    
+    
 }
