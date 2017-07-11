cef1067b755254f86ce4b8536ff3b6bd04c37170
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-11 10:33:42 -0700

    Add tests to GenderTest

--- tests/Arabic/GenderTest.php
+++ tests/Arabic/GenderTest.php
@@ -25,4 +25,18 @@ class GenderTest extends AbstractTestCase
         $this->assertInstanceOf(I18N_Arabic_Gender::class, $this->gender->myObject);
     }
     
+    /** @test */
+    public function it_detects_the_gender_of_given_name()
+    {
+        $this->assertTrue($this->gender->isFemale('خديجة بن قنة'));
+        $this->assertTrue($this->gender->isFemale('لينا زهر الدين'));
+        $this->assertTrue($this->gender->isFemale('ابتهال'));
+        $this->assertTrue($this->gender->isFemale('فيروز زياني'));
+        $this->assertTrue($this->gender->isFemale('جلنار موسى'));
+        $this->assertFalse($this->gender->isFemale('فيصل القاسم'));
+        $this->assertFalse($this->gender->isFemale('المعز بو لحية'));
+        $this->assertFalse($this->gender->isFemale('خالد صالح'));
+        $this->assertFalse($this->gender->isFemale('علي الظفيري'));
+    }
+    
 }
