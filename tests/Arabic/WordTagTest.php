a7be38b81d62059e0a5555214eba1196815f3b37
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-10 23:57:47 -0700

    Add WordTag test

---
+++ tests/Arabic/WordTagTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Arabic;
+
+use I18N_Arabic_WordTag;
+use Tests\AbstractTestCase;
+
+class WordTagTest extends AbstractTestCase
+{
+
+    /**
+     * @var I18N_Arabic_WordTag
+     */
+    protected $wordTag;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->wordTag = new \I18N_Arabic('WordTag');
+    }
+
+    /** @test */
+    public function it_loads_normalise_class()
+    {
+        $this->assertInstanceOf(I18N_Arabic_WordTag::class, $this->wordTag->myObject);
+    }
+    
+}
