891568af56928bd2661b1330c8107cbed611d694
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-11 01:39:27 -0700

    test standard method

--- tests/Arabic/StandardTest.php
+++ tests/Arabic/StandardTest.php
@@ -25,5 +25,26 @@ class StandardTest extends AbstractTestCase
         $this->assertInstanceOf(I18N_Arabic_Standard::class, $this->standard->myObject);
     }
     
-    
+    /** @test */
+    public function it_fix_arabic_text()
+    {
+        $text = <<<END
+هذا نص عربي ، و فيه علامات ترقيم بحاجة إلى ضبط و معايرة !و كذلك نصوص( بين
+أقواس )أو حتى مؤطرة"بإشارات إقتباس "أو- علامات إعتراض -الخ......
+<br>
+لذا ستكون هذه المكتبة أداة و وسيلة لمعالجة مثل هكذا حالات، بما فيها الواحدات 1
+Kg أو مثلا MB 16 وسواها حتى النسب المؤية مثل 20% أو %50 وهكذا ...
+END;
+        $expectedText = <<<END
+هذا نص عربي، وفيه علامات ترقيم بحاجة إلى ضبط ومعايرة! وكذلك نصوص (بين
+أقواس) أو حتى مؤطرة "بإشارات إقتباس" أو -علامات إعتراض- الخ...
+<br>
+لذا ستكون هذه المكتبة أداة و وسيلة لمعالجة مثل هكذا حالات، بما فيها الواحدات 1
+Kg أو مثلا <span dir="ltr">16 MB</span> وسواها حتى النسب المؤية مثل %20 أو %50 وهكذا...
+END;
+        
+        $actualText = $this->standard->standard($text);
+        
+        $this->assertEquals($expectedText, $actualText);
+    }
 }
