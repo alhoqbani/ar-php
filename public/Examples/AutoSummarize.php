4b6ae6d29b82cb64a7e0079fc4f2cb234681422e
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-11 22:07:26 -0700

    fix example AutoSummarize

--- public/Examples/AutoSummarize.php
+++ public/Examples/AutoSummarize.php
@@ -1,44 +1,45 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+use ArUtil\Arabic;
 
+?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>Arabic Auto Summarize Class</title>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="style.css" media="all" />
-
-<style type="text/css">
-.summary { background-color:#eeee80; }
-</style>
-
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Arabic Auto Summarize Class</title>
 </head>
 
 <body>
 
-<div class="Paragraph" dir="rtl">
-<h2 dir="ltr">Example Output:</h2>
-<?php
-/**
- * Example of Arabic Auto Summarize Class
- *
- * @category  I18N
- * @package   I18N_Arabic
- * @author    Khaled Al-Sham'aa <khaled@ar-php.org>
- * @copyright 2006-2016 Khaled Al-Sham'aa
- *
- * @license   LGPL <http://www.gnu.org/licenses/lgpl.txt>
- * @link      http://www.ar-php.org
- */
-
-error_reporting(E_ALL);
-$time_start = microtime(true);
-
-require '../../Arabic.php';
-/** @var \AutoSummarize $Arabic */
-$Arabic = new I18N_Arabic('AutoSummarize');
-
-$rate     = 25;
-$title    = 'أضخم تجربة علمية لدراسة بنية المادة المعتمة بمصادم الهدرونات الكبير';
-$contents = <<<END
+<div class="Paragraph " dir="rtl">
+    <h1 dir="ltr">AutoSummarize</h1>
+    <h2 dir="ltr">Example Output:</h2>
+    <?php
+    /**
+     * Example of Arabic Auto Summarize Class
+     *
+     * @category  I18N
+     * @package   I18N_Arabic
+     * @author    Khaled Al-Sham'aa <khaled@ar-php.org>
+     * @copyright 2006-2016 Khaled Al-Sham'aa
+     *
+     * @license   LGPL <http://www.gnu.org/licenses/lgpl.txt>
+     * @link      http://www.ar-php.org
+     */
+    
+    error_reporting(E_ALL);
+    $time_start = microtime(true);
+    
+    /** @var \ArUtil\AutoSummarize $Arabic */
+    $Arabic = new Arabic('AutoSummarize');
+    
+    $rate = 25;
+    $title = 'أضخم تجربة علمية لدراسة بنية المادة المعتمة بمصادم الهدرونات الكبير';
+    $contents = <<<END
 قال علماء في مركز أبحاث الفيزياء التابع للمنظمة الأوروبية للابحاث النووية يوم الجمعة
 أنهم حققوا تصادما بين جسيمات بكثافة قياسية في إنجاز مهم في برنامجهم لكشف أسرار الكون. 
 وجاء التطور في الساعات الأولى بعد تغذية مصادم الهدرونات الكبير بحزمة أشعة بها 
@@ -78,24 +79,25 @@ $contents = <<<END
 المنظمة بأنه "الفيزياء الجديدة" بما يدفع المعرفة لتجاوز ما يسمى النموذج المعياري 
 المعتمد على نظريات العالم البرت اينشتاين في اوائل القرن العشرين.
 END;
-
+    
     $contents = str_replace("\n", '', $contents);
-    $q = isset($_GET['q']) ? $_GET['q'] : null;
+    $q = isset($_GET['q']) ? $_GET['q'] : '';
     $highlighted = $Arabic->highlightRateSummary($contents, $rate, $q, 'summary');
     $summary = $Arabic->doRateSummarize($contents, $rate, $q);
-
+    
     echo "<h3>$title:</h3>";
     echo '<a href="AutoSummarize.php">ملخص عادي</a> | ';
     echo '<a href="AutoSummarize.php?q=هيجنز">';
     echo 'الملخص لو كنت تبحث عن كلمة هيجنز</a>';
     echo "<h4>الملخص</h4>$summary";
     echo "<h4>النص الكامل</h4>$highlighted";
-?>
-</div><br />
+    ?>
+</div>
+<br/>
 <div class="Paragraph">
-<h2>Example Code:</h2>
-<?php
-$code = <<< ENDALL
+    <h2>Example Code:</h2>
+    <?php
+    $code = <<< ENDALL
 <?php
     require '../../Arabic.php';
     \$Arabic = new I18N_Arabic('AutoSummarize');
@@ -155,25 +157,25 @@ END;
     echo "<h4>الملخص</h4>\$summary";
     echo "<h4>النص الكامل</h4>\$highlighted";
 ENDALL;
-
-highlight_string($code);
-
-$time_end = microtime(true);
-$time = $time_end - $time_start;
-
-echo "<hr />Total execution time is $time seconds<br />\n";
-echo 'Amount of memory allocated to this script is ' . memory_get_usage() . ' bytes';
-
-$included_files = get_included_files();
-echo '<h4>Names of included or required files:</h4><ul>';
-
-foreach ($included_files as $filename) {
-    echo "<li>$filename</li>";
-}
-
-echo '</ul>';
-?>
-<a href="../Docs/I18N_Arabic/_Arabic---AutoSummarize.php.html" target="_blank">Related Class Documentation</a>
+    
+    highlight_string($code);
+    
+    $time_end = microtime(true);
+    $time = $time_end - $time_start;
+    
+    echo "<hr />Total execution time is $time seconds<br />\n";
+    echo 'Amount of memory allocated to this script is ' . memory_get_usage() . ' bytes';
+    
+    $included_files = get_included_files();
+    echo '<h4>Names of included or required files:</h4><ul>';
+    
+    foreach ($included_files as $filename) {
+        echo "<li>$filename</li>";
+    }
+    
+    echo '</ul>';
+    ?>
+    <a href="../Docs/I18N_Arabic/_Arabic---AutoSummarize.php.html" target="_blank">Related Class Documentation</a>
 </div>
 </body>
 </html>
