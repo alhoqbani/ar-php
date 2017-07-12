a49a5976813f2ea244a1dd6e9eb9c2f2988a6ba5
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-12 14:10:59 -0700

    Fix Soundex example

--- public/Examples/Soundex.php
+++ public/Examples/Soundex.php
@@ -1,39 +1,52 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-<title>Arabic Soundex Class</title>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="style.css" media="all" />
-</head>
-
-<body>
-
-<div class="Paragraph">
-<h2>Example Output:</h2>
 <?php
-/**
- * Example of Arabic Soundex Class
- *
- * @category  I18N
- * @package   I18N_Arabic
- * @author    Khaled Al-Sham'aa <khaled@ar-php.org>
- * @copyright 2006-2016 Khaled Al-Sham'aa
- *
- * @license   LGPL <http://www.gnu.org/licenses/lgpl.txt>
- * @link      http://www.ar-php.org
- */
 
-error_reporting(E_ALL);
-$time_start = microtime(true);
+use ArUtil\Arabic;
 
-require '../../Arabic.php';
-$Arabic = new I18N_Arabic('Soundex');
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-$Clinton = array('كلينتون', 'كلينتن', 'كلينطون', 'كلنتن', 'كلنتون', 
-                 'كلاينتون');
-
-echo <<<END
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Arabic Soundex Class</title>
+</head>
+<body>
+<div class="Paragraph">
+    <h1 dir="ltr">Soundex</h1>
+    <h2>Example Output:</h2>
+    <?php
+    /**
+     * Example of Arabic Soundex Class
+     *
+     * @category  I18N
+     * @package   I18N_Arabic
+     * @author    Khaled Al-Sham'aa <khaled@ar-php.org>
+     * @copyright 2006-2016 Khaled Al-Sham'aa
+     *
+     * @license   LGPL <http://www.gnu.org/licenses/lgpl.txt>
+     * @link      http://www.ar-php.org
+     */
+    
+    error_reporting(E_ALL);
+    $time_start = microtime(true);
+    
+    /** @var \ArUtil\Soundex $Arabic */
+    $Arabic = new Arabic('Soundex');
+    
+    $Clinton = [
+        'كلينتون',
+        'كلينتن',
+        'كلينطون',
+        'كلنتن',
+        'كلنتون',
+        'كلاينتون',
+    ];
+    
+    echo <<<END
 <table border="0" cellpadding="5" cellspacing="2" align="center">
 <tr>
     <td colspan="3">Listed below are 6 different spelling for the name
@@ -46,27 +59,27 @@ echo <<<END
     <td bgcolor=#000000 width=33%><b><font color=#ffffff>Output</font></b></td>
 </tr>
 END;
-echo '<tr>
+    echo '<tr>
         <td bgcolor=#f5f5f5>PHP soundex function</td>
         <td bgcolor=#f5f5f5>Clinton</td>
         <td bgcolor=#f5f5f5>' . soundex('Clinton') . '</td>
       </tr>';
-
-foreach ($Clinton as $name) {
-    echo '<tr>
+    
+    foreach ($Clinton as $name) {
+        echo '<tr>
             <td bgcolor=#f5f5f5>ArSoundex Method</td>
             <td bgcolor=#f5f5f5>' . $name . '</td>
             <td bgcolor=#f5f5f5>' . $Arabic->soundex($name) . '</td>
           </tr>';
-}
-
-echo '<tr>
+    }
+    
+    echo '<tr>
         <td bgcolor=#f5f5c5>ArSoundex Method</td>
         <td bgcolor=#f5f5c5>كلينزمان</td>
         <td bgcolor=#f5f5c5>' . $Arabic->soundex('كلينزمان') . '</td>
       </tr>';
-
-echo <<<END
+    
+    echo <<<END
 <tr>
     <td colspan=3>&nbsp;</td>
 </tr>
@@ -83,34 +96,41 @@ echo <<<END
 <tr>
 END;
     
-$Milosevic = array('ميلوسيفيتش', 'ميلوسفيتش', 'ميلوزفيتش', 'ميلوزيفيتش',
-                   'ميلسيفيتش', 'ميلوسيفتش');
-
-echo '<tr>
+    $Milosevic = [
+        'ميلوسيفيتش',
+        'ميلوسفيتش',
+        'ميلوزفيتش',
+        'ميلوزيفيتش',
+        'ميلسيفيتش',
+        'ميلوسيفتش',
+    ];
+    
+    echo '<tr>
         <td bgcolor=#f5f5f5>PHP soundex function</td>
         <td bgcolor=#f5f5f5>Milosevic</td>
         <td bgcolor=#f5f5f5>' . soundex('Milosevic') . '</td>
       </tr>';
-                   
-foreach ($Milosevic as $name) {
-    echo '<tr>
+    
+    foreach ($Milosevic as $name) {
+        echo '<tr>
             <td bgcolor=#f5f5f5>ArSoundex Method</td>
             <td bgcolor=#f5f5f5>' . $name . '</td>
             <td bgcolor=#f5f5f5>' . $Arabic->soundex($name) . '</td>
           </tr>';
-}
-
-echo '<tr>
+    }
+    
+    echo '<tr>
         <td bgcolor=#f5f5c5>ArSoundex Method</td>
         <td bgcolor=#f5f5c5>ميلينيوم</td>
         <td bgcolor=#f5f5c5>' . $Arabic->soundex('ميلينيوم') . '</td>
       </tr></table>';
-?>
-</div><br />
+    ?>
+</div>
+<br/>
 <div class="Paragraph">
-<h2>Example Code:</h2>
-<?php
-$code = <<< ENDALL
+    <h2>Example Code:</h2>
+    <?php
+    $code = <<< ENDALL
 <?php
     require '../../Arabic.php';
     \$Arabic = new I18N_Arabic('Soundex');
@@ -189,26 +209,25 @@ END;
             <td bgcolor=#f5f5c5>' . \$Arabic->soundex('ميلينيوم') . '</td>
           </tr></table>';
 ENDALL;
-
-highlight_string($code);
-
-$time_end = microtime(true);
-$time = $time_end - $time_start;
-
-echo "<hr />Total execution time is $time seconds<br />\n";
-echo 'Amount of memory allocated to this script is ' . memory_get_usage() . ' bytes';
-
-$included_files = get_included_files();
-echo '<h4>Names of included or required files:</h4><ul>';
-
-foreach ($included_files as $filename) {
-    echo "<li>$filename</li>";
-}
-
-echo '</ul>';
-?>
-<a href="../Docs/I18N_Arabic/_Arabic---Soundex.php.html" target="_blank">Related Class Documentation</a>
+    
+    highlight_string($code);
+    
+    $time_end = microtime(true);
+    $time = $time_end - $time_start;
+    
+    echo "<hr />Total execution time is $time seconds<br />\n";
+    echo 'Amount of memory allocated to this script is ' . memory_get_usage() . ' bytes';
+    
+    $included_files = get_included_files();
+    echo '<h4>Names of included or required files:</h4><ul>';
+    
+    foreach ($included_files as $filename) {
+        echo "<li>$filename</li>";
+    }
+    
+    echo '</ul>';
+    ?>
+    <a href="../Docs/I18N_Arabic/_Arabic---Soundex.php.html" target="_blank">Related Class Documentation</a>
 </div>
-</center>
 </body>
 </html>
