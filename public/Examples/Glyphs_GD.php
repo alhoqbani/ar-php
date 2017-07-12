6b400192b4ec406b9d74215a87ba3e9bb6618095
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-12 14:28:48 -0700

    Fix Glyphs Gd

--- public/Examples/Glyphs_GD.php
+++ public/Examples/Glyphs_GD.php
@@ -1,6 +1,11 @@
 <?php
+
+use ArUtil\Arabic;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
 /**
- * Example of GD implementation for Arabic glyphs Class 
+ * Example of GD implementation for Arabic glyphs Class
  *
  * @category  I18N
  * @package   I18N_Arabic
@@ -21,15 +26,15 @@ $im = @imagecreatefromgif('GD/bg.gif');
 
 // Create some colors
 $black = imagecolorallocate($im, 0, 0, 0);
-$blue  = imagecolorallocate($im, 0, 0, 255);
+$blue = imagecolorallocate($im, 0, 0, 255);
 $white = imagecolorallocate($im, 255, 255, 255);
 
 // Replace by your own font full path and name
 $path = substr(
-    $_SERVER['SCRIPT_FILENAME'], 0, 
+    $_SERVER['SCRIPT_FILENAME'], 0,
     strrpos($_SERVER['SCRIPT_FILENAME'], '/')
 );
-$font = $path.'/GD/ae_AlHor.ttf';
+$font = $path . '/GD/ae_AlHor.ttf';
 
 // UTF-8 charset
 $text = 'بسم الله الرحمن الرحيم';
@@ -37,8 +42,8 @@ imagefill($im, 0, 0, $white);
 imagettftext($im, 20, 0, 10, 50, $blue, $font, 'UTF-8:');
 imagettftext($im, 20, 0, 250, 50, $black, $font, $text);
 
-require '../../Arabic.php';
-$Arabic = new I18N_Arabic('Glyphs');
+/** @var \ArUtil\Glyphs $Arabic */
+$Arabic = new Arabic('Glyphs');
 
 $text = 'بسم الله الرحمن الرحيم';
 $text = $Arabic->utf8Glyphs($text);
