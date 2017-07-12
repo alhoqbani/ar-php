5bd85c3ab5d3c2a09c6709bc5fbddd0028ed747a
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-11 22:46:10 -0700

    Gender example

--- public/Examples/Gender.php
+++ public/Examples/Gender.php
@@ -1,15 +1,21 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+use ArUtil\Arabic;
 
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>Arabic Gender Guesser</title>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="style.css" media="all" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Arabic Gender Guesser</title>
 </head>
-
 <body>
-
 <div class="Paragraph">
+<h1>Gender</h1>
 <h2>Example Output:</h2>
 <?php
 /**
@@ -27,8 +33,8 @@
 error_reporting(E_ALL);
 $time_start = microtime(true);
 
-require '../../Arabic.php';
-$Arabic = new I18N_Arabic('Gender');
+/** @var \ArUtil\Gender $Arabic */
+$Arabic = new Arabic('Gender');
 
 $names = array('أحمد بشتو','أحمد منصور','الحبيب الغريبي','المعز بو لحية',
                   'توفيق طه','جلنار موسى','جمال  ريان','جمانة نمور',
