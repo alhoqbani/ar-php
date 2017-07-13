b9618db8e9c3f0b5df0fa33d8c62b97b927c4719
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-13 04:17:28 -0700

    Add method to set toady hijri date, and set magic get and set

--- src/ArUtil/Time/ArDateTime.php
+++ src/ArUtil/Time/ArDateTime.php
@@ -5,7 +5,13 @@ namespace ArUtil\Time;
 use ArUtil\I18N\Date;
 use ArUtil\I18N\Mktime;
 use Carbon\Carbon;
+use InvalidArgumentException;
 
+/**
+ * @property integer arDay
+ * @property integer arMonth
+ * @property integer arYear
+ */
 class ArDateTime extends Carbon
 {
     
@@ -17,6 +23,13 @@ class ArDateTime extends Carbon
         
         return $instance;
     }
+
+    public function __construct($time = null, $tz = null)
+    {
+        $this->setTodayHijriDate();
+
+        parent::__construct($time, $tz);
+    }
     
     protected function hijriToTimestamp($arYear, $arMonth, $arDay)
     {
@@ -24,7 +37,7 @@ class ArDateTime extends Carbon
             $correction = 0);
     }
     
-    protected function convertTodayToHijri()
+    private function convertTodayToHijri()
     {
         list($arYear, $arMonth, $arDay) = (new Date())->hjConvert(date('Y'), date('m'), date('d'));
         
@@ -34,5 +47,47 @@ class ArDateTime extends Carbon
             'arDay'   => $arDay,
         ];
     }
+    
+    private function setTodayHijriDate()
+    {
+        $today = $this->convertTodayToHijri();
+        $this->arYear = (int)$today['arYear'];
+        $this->arMonth = (int)$today['arMonth'];
+        $this->arDay = (int)$today['arDay'];
+    }
+    
+    public function __get($name)
+    {
+        switch ($name) {
+            case $name === 'arYear':
+                return $this->arYear;
+            case $name === 'arMonth':
+                return $this->arMonth;
+            case $name === 'arDay':
+                return $this->arDay;
+            default:
+                break;
+        }
+        return parent::__get($name);
+    }
+    
+    public function __set($name, $value)
+    {
+        try {
+            parent::__set($name, $value);
+        } catch (InvalidArgumentException $e) {
+            switch ($name) {
+                case 'arYear':
+                case 'arMonth':
+                case 'arDay':
+                    $this->{$name} = $value;
+                    break;
+                default:
+                    throw $e;
+            }
+        }
+        
+    }
+    
     
 }
--- tests/Time/ArDateTimeTest.php
+++ tests/Time/ArDateTimeTest.php
@@ -2,8 +2,10 @@
 
 namespace ArUtil\Tests\Time;
 
-use Carbon\Carbon;
+use ArUtil\ArUtil;
+use ArUtil\I18N\Date;
 use ArUtil\Time\ArDateTime;
+use Carbon\Carbon;
 
 class ArDateTimeTest extends TimeAbstractTest
 {
@@ -20,5 +22,29 @@ class ArDateTimeTest extends TimeAbstractTest
     public function it_extends_carbon()
     {
         $this->assertInstanceOf(Carbon::class, new ArDateTime());
+    }
+    
+    /** @test */
+    public function it_return_today_hijri_date()
+    {
+        list($arYear, $arMonth, $arDay) = (new Date())->hjConvert(date('Y'), date('m'), date('d'));
+        $arD = ArUtil::date();
+        $this->assertEquals($arDay, $arD->arDay);
+    }
+    
+    /** @test */
+    public function it_return_today_hijri_month()
+    {
+        list($arYear, $arMonth, $arDay) = (new Date())->hjConvert(date('Y'), date('m'), date('d'));
+        $arD = ArUtil::date();
+        $this->assertEquals($arMonth, $arD->arMonth);
+    }
+    
+    /** @test */
+    public function it_return_today_hijri_year()
+    {
+        list($arYear, $arMonth, $arDay) = (new Date())->hjConvert(date('Y'), date('m'), date('d'));
+        $arD = ArUtil::date();
+        $this->assertEquals($arYear, $arD->arYear);
     }
 }
