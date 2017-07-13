c67520a8b8ce691fd005a7a92f45a5fd73926332
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-13 04:25:01 -0700

    Add helper method to set today hijri date for testing

--- tests/Time/TimeAbstractTest.php
+++ tests/Time/TimeAbstractTest.php
@@ -2,6 +2,7 @@
 
 namespace ArUtil\Tests\Time;
 
+use ArUtil\I18N\Date;
 use ArUtil\Tests\AbstractTestCase;
 use Carbon\Carbon;
 
@@ -16,22 +17,39 @@ abstract class TimeAbstractTest extends AbstractTestCase
     /**
      * @var string Saved timezone before test
      */
-    protected $saveTz;
+    protected $savedTz;
+    /**
+     * @var array Today Hijri Date to use in testing.
+     */
+    protected $arToday;
     
     protected function setUp()
     {
         parent::setUp();
-        $this->saveTz = date_default_timezone_get();
+        $this->savedTz = date_default_timezone_get();
         
         date_default_timezone_set('America/Los_Angeles');
         
         Carbon::setTestNow($this->now = Carbon::now());
+        $this->setTodayHijriDate();
     }
     
     protected function tearDown()
     {
-        date_default_timezone_set($this->saveTz);
+        date_default_timezone_set($this->savedTz);
         Carbon::setTestNow();
+        unset($this->arToday, $this->savedTz, $this->now);
         parent::tearDown();
+    }
+    
+    private function setTodayHijriDate()
+    {
+        list($arYear, $arMonth, $arDay) = (new Date())->hjConvert(date('Y'), date('m'), date('d'));
+        
+        $this->arToday = [
+            'arYear'  => $arYear,
+            'arMonth' => $arMonth,
+            'arDay'   => $arDay,
+        ];
     }
 }
