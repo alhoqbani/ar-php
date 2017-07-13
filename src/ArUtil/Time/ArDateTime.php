1b5e4ce47422a8bdf00fb8866520c1a4e6b68a37
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-13 03:47:20 -0700

    Add method to convert today's date to hijri

--- src/ArUtil/Time/ArDateTime.php
+++ src/ArUtil/Time/ArDateTime.php
@@ -2,6 +2,7 @@
 
 namespace ArUtil\Time;
 
+use ArUtil\I18N\Date;
 use ArUtil\I18N\Mktime;
 use Carbon\Carbon;
 
@@ -21,6 +22,17 @@ class ArDateTime extends Carbon
     {
         return (new Mktime())->mktime(date('H'), date('i'), date('s'), $arMonth, $arDay, $arYear,
             $correction = 0);
+    }
+    
+    protected function convertTodayToHijri()
+    {
+        list($arYear, $arMonth, $arDay) = (new Date())->hjConvert(date('Y'), date('m'), date('d'));
+        
+        return [
+            'arYear'  => $arYear,
+            'arMonth' => $arMonth,
+            'arDay'   => $arDay,
+        ];
     }
     
 }
