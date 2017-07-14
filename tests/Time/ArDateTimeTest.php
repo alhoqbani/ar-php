af5927ea8655fb2f2bfed1f4e7849b5d48af8170
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-13 20:48:33 -0700

    Add test

--- tests/Time/ArDateTimeTest.php
+++ tests/Time/ArDateTimeTest.php
@@ -24,14 +24,18 @@ class ArDateTimeTest extends AbstractTimeTest
     }
     
     /** @test */
+    public function it_works()
+    {
+        $arD = ArUtil::date()->arCreate(1405, 8, 10, 2, 10, 50);
+        $this->assertArDateTime($arD, 1405, 8, 10, 2, 10, 50);
+    }
+    
+    /** @test */
     public function it_return_carbon_instance_from_hijri_string_date()
     {
-        /** @var Carbon $arD */
         $arD = ArUtil::date()->arCreateFromDate(1405, 8, 10);
         $this->assertInstanceOf(Carbon::class, $arD);
         $this->assertArDateTime($arD, 1405, 8, 10);
         $this->assertEquals('1985-05-01', $arD->toDateString());
     }
-    
-    
 }
