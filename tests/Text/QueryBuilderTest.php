f03a3c1c62f4ce37c2d79c6f4237b30dc6f948a5
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-15 12:01:47 -0700

    Change test to use QueryBuilder as proporty

--- tests/Text/QueryBuilderTest.php
+++ tests/Text/QueryBuilderTest.php
@@ -3,10 +3,29 @@
 namespace ArUtil\Tests\Text;
 
 use ArUtil\ArUtil;
+use ArUtil\I18N\Arabic;
 use ArUtil\Tests\AbstractTestCase;
 
 class QueryBuilderTest extends AbstractTestCase
 {
+    
+    /**
+     * @var \ArUtil\Text\QueryBuilder
+     */
+    protected $arQ;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        new Arabic('Numbers');
+        $this->arQ = ArUtil::query();
+    }
+    
+    protected function tearDown()
+    {
+        unset($this->arQ);
+        parent::tearDown();
+    }
     
     /** @test */
     public function it_prepares_single_where_regexp_statement()
@@ -17,8 +36,8 @@ class QueryBuilderTest extends AbstractTestCase
             'pattern' => 'book',
             'boolean' => 'and',
         ];
-        $query = ArUtil::query()->whereReg('title', 'book');
-        $actualStatement = $query->getWheresReg()[0];
+        $this->arQ->whereReg('title', 'book');
+        $actualStatement = $this->arQ->getWheresReg()[0];
         
         $this->assertEquals($expectedStatement, $actualStatement);
         
@@ -27,28 +46,29 @@ class QueryBuilderTest extends AbstractTestCase
     /** @test */
     public function it_prepares_multiple_where_regexp_statements()
     {
-        $query = ArUtil::query()->whereReg('title', 'book');
-        $query->whereReg('title', 'book');
-        $query->whereReg('title', 'book');
+        $this->arQ->whereReg('title', 'book');
+        $this->arQ->whereReg('title', 'book');
+        $this->arQ->whereReg('title', 'book');
         
-        $this->assertCount(3, $query->getWheresReg());
+        $this->assertCount(3, $this->arQ->getWheresReg());
     }
     
     /** @test */
     public function it_assigns_the_boolean_operator()
     {
-        $query = ArUtil::query()->whereReg('title', 'book');
-        $this->assertEquals('and', $query->getWheresReg()[0]['boolean']);
-        
-        $query = ArUtil::query()->whereReg('title', 'book', 'or');
-        $this->assertEquals('or', $query->getWheresReg()[0]['boolean']);
+        $this->arQ->whereReg('title', 'book');
+        $this->assertEquals('and', $this->arQ->getWheresReg()[0]['boolean']);
+    
+        $this->arQ->whereReg('title', 'book', 'or');
+        $this->assertEquals('or', $this->arQ->getWheresReg()[1]['boolean']);
     }
     
     /** @test */
     public function it_splits_sentences_into_multiple_clauses()
     {
-        $q = ArUtil::query()->whereReg('title', 'مؤتمر أبل للمطورين');
-           $wheres =  $q->getWheresReg();
+        $this->arQ->whereReg('title', 'مؤتمر أبل للمطورين');
+        $wheres = $this->arQ->getWheresReg();
+
         $this->assertCount(3, $wheres);
     }
     
@@ -57,9 +77,9 @@ class QueryBuilderTest extends AbstractTestCase
     public function it_regexpy_all_values_of_where_statement()
     {
         $expectedPattern = '(ا|أ|إ|آ)بل';
-        
-        $query = ArUtil::query()->whereReg('title', 'أبل');
-        $actualPattern = $query->getWheresReg()[0]['pattern'];
+    
+        $this->arQ->whereReg('title', 'أبل');
+        $actualPattern = $this->arQ->getWheresReg()[0]['pattern'];
         
         $this->assertEquals($expectedPattern, $actualPattern);
         
@@ -69,7 +89,7 @@ class QueryBuilderTest extends AbstractTestCase
     public function it_returns_regexp_pattern_for_single_word()
     {
         $expectedPattern = '(ا|أ|إ|آ)بل';
-        $actualPattern = ArUtil::query()->regexpy('أبل');
+        $actualPattern = $this->arQ->regexpy('أبل');
         
         $this->assertEquals($expectedPattern, $actualPattern);
     }
@@ -81,7 +101,7 @@ class QueryBuilderTest extends AbstractTestCase
             '(ا|أ|إ|آ)بل',
             'للح(ا|أ|إ|آ)سب(ة|(ا|أ|إ|آ)ت)?',
         ];
-        $actualPattern = ArUtil::query()->regexpy('أبل للحاسبات');
+        $actualPattern = $this->arQ->regexpy('أبل للحاسبات');
         
         $this->assertEquals($expectedRegex, $actualPattern);
     }
@@ -93,7 +113,7 @@ class QueryBuilderTest extends AbstractTestCase
             '(ا|أ|إ|آ)بل',
             'للح(ا|أ|إ|آ)سب(ة|(ا|أ|إ|آ)ت)?',
         ];
-        $actualPattern = ArUtil::query()->regexpy(['أبل', 'للحاسبات']);
+        $actualPattern = $this->arQ->regexpy(['أبل', 'للحاسبات']);
         
         $this->assertEquals($expectedRegex, $actualPattern);
     }
