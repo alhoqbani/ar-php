b941773719b525a8ae63f3b14fb1df1acd07843c
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-27 03:59:32 -0700

    write test for updating Hijri date

---
+++ tests/Time/UpdateHijriDateTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ArUtil\Tests\Time;
+
+use ArUtil\ArUtil;
+use ArUtil\Time\ArDateTime;
+use Carbon\Carbon;
+
+class UpdateHijriDateTest extends AbstractTimeTest
+{
+    
+    /** @test */
+    public function it_updates_its_hijri_date_when_carbon_dates_changes()
+    {
+        $arD = ArUtil::date()->arCreateFromDate(1438, 10, 15);
+        $this->assertCarbon($arD, 2017, 7, 10);
+        
+        $arD->addDays(10);
+
+	    $this->assertArDateTime($arD, 1438, 10, 25);
+    }
+}
